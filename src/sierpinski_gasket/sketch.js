7fbfa9b015df4365e2de2a873e158813cab3f298
Gen Ichino <zenoplex@gmail.com>
2017-11-30 08:24:19 +0900

    refactor: add drawing function

--- src/sierpinski_gasket/sketch.js
+++ src/sierpinski_gasket/sketch.js
@@ -18,38 +18,25 @@ const a = new Point(center.x - radius, center.y + radius); // bottom left
 const b = new Point(center.x, center.y - radius); // top
 const c = new Point(center.x + radius, center.y + radius); // bottom right
 
-const points = sierpinski.draw(a, b, c)(8);
-
-const draw = (list) => {
+type Draw = (any[]) => void;
+const draw: Draw = (list) => {
   const len = list.length;
   for (let i = 0; i < len; i++) {
     const element = list[i];
 
     if (Array.isArray(element)) draw(element);
 
-    graphic.lineStyle(2, 0xffffff);
-
+    graphic.lineStyle(1, 0xffffff);
+    graphic.beginFill(0xffffff);
     if (i === 0) graphic.moveTo(element.x, element.y);
     else graphic.lineTo(element.x, element.y);
-    if (i === len - 1) graphic.closePath();
+    if (i === len - 1) graphic.lineTo(list[0].x, list[0].y);
   }
+  graphic.endFill();
 };
 
 const render = () => {
-  // graphic.clear();
-
-  // const len = points.length;
-  // for (let i = 0; i < len; i++) {
-  //   const point = points[i];
-  //   if (i === 0) graphic.moveTo(point.x, point.y);
-  //   else graphic.lineTo(point.x, point.y);
-  // }
-  // graphic.closePath();
-  // // graphic.endFill();
-  // container.scale.set(10, 10);
+  const points = sierpinski.draw(a, b, c)(7);
   draw(points);
 };
-// app.ticker.add(render);
-
 render();
-//
