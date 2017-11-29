41156149d85adc418b41e062439f537e83e2f104
Gen Ichino <zenoplex@gmail.com>
2017-11-29 09:59:32 +0900

    feat: draw simple triangle

--- src/sierpinski_gasket/Sierspinski.js
+++ src/sierpinski_gasket/Sierspinski.js
@@ -1,14 +1,22 @@
 // @flow
 import * as math from '@gen/math';
 
-const drawTriangle = (depth) => {
+type Point = {
+  x: number,
+  y: number,
+};
+
+type DrawTriangle = number => Point[];
+export const drawTriangle: DrawTriangle = (depth = 0) => {
   const points = [];
-  const angle = -Math.PI / 2;
+  let angle = -Math.PI / 2;
   if (depth === 0) {
+    points.push({ x: math.cos(angle), y: math.sin(angle) });
+    angle += Math.PI * 2 / 3;
+    points.push({ x: math.cos(angle), y: math.sin(angle) });
+    angle += Math.PI * 2 / 3;
     points.push({ x: math.cos(angle), y: math.sin(angle) });
   }
 
   return points;
 };
-
-console.log(drawTriangle(0));
