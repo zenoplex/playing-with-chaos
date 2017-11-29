a105df33f8646b5851488eea71bf7337e63be4fb
Gen Ichino <zenoplex@gmail.com>
2017-11-30 02:13:26 +0900

    feat: draw gasket

--- src/sierpinski_gasket/sketch.js
+++ src/sierpinski_gasket/sketch.js
@@ -2,34 +2,54 @@
 import PIXI from 'pixi.js';
 import * as canvas from '../common/canvas';
 import * as sierpinski from './Sierspinski';
+import { Point } from './Point';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 const app = canvas.setup({ width, height, background: 0 });
 const container = new PIXI.Container();
 const graphic = new PIXI.Graphics();
-graphic.lineStyle(2, 0xffffff);
 container.addChild(graphic);
-container.x = width / 2;
-container.y = height / 2;
 app.stage.addChild(container);
 
-const points = sierpinski.drawTriangle();
+const radius = Math.min(width, height) / 2;
+const center = new Point(width / 2, height / 2);
+const a = new Point(center.x - radius, center.y + radius); // bottom left
+const b = new Point(center.x, center.y - radius); // top
+const c = new Point(center.x + radius, center.y + radius); // bottom right
+
+const points = sierpinski.draw(a, b, c)(8);
+
+const draw = (list) => {
+  const len = list.length;
+  for (let i = 0; i < len; i++) {
+    const element = list[i];
+
+    if (Array.isArray(element)) draw(element);
+
+    graphic.lineStyle(2, 0xffffff);
+
+    if (i === 0) graphic.moveTo(element.x, element.y);
+    else graphic.lineTo(element.x, element.y);
+    if (i === len - 1) graphic.closePath();
+  }
+};
 
 const render = () => {
-  graphic.clear();
-  graphic.beginFill(0xfffffff);
+  // graphic.clear();
 
-  const len = points.length;
-  for (let i = 0; i < len; i++) {
-    const point = points[i];
-    if (i === 0) graphic.moveTo(point.x, point.y);
-    else graphic.lineTo(point.x, point.y);
-  }
-  graphic.closePath();
-  // graphic.endFill();
-  container.scale.set(10, 10);
+  // const len = points.length;
+  // for (let i = 0; i < len; i++) {
+  //   const point = points[i];
+  //   if (i === 0) graphic.moveTo(point.x, point.y);
+  //   else graphic.lineTo(point.x, point.y);
+  // }
+  // graphic.closePath();
+  // // graphic.endFill();
+  // container.scale.set(10, 10);
+  draw(points);
 };
 // app.ticker.add(render);
 
 render();
+//
