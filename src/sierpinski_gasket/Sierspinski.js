7a55e8f09178ac30bebe518638bac5eea3237cb8
Gen Ichino <zenoplex@gmail.com>
2017-11-30 02:13:17 +0900

    feat: add prototype

--- src/sierpinski_gasket/Sierspinski.js
+++ src/sierpinski_gasket/Sierspinski.js
@@ -1,22 +1,26 @@
 // @flow
 import * as math from '@gen/math';
+import { Point } from './Point';
 
-type Point = {
-  x: number,
-  y: number,
-};
+type MidPoint = (Point, Point) => Point;
+const midPoint: MidPoint = (a, b) =>
+  new Point((a.x + b.x) / 2, (a.y + b.y) / 2);
 
-type DrawTriangle = number => Point[];
-export const drawTriangle: DrawTriangle = (depth = 0) => {
-  const points = [];
-  let angle = -Math.PI / 2;
-  if (depth === 0) {
-    points.push({ x: math.cos(angle), y: math.sin(angle) });
-    angle += Math.PI * 2 / 3;
-    points.push({ x: math.cos(angle), y: math.sin(angle) });
-    angle += Math.PI * 2 / 3;
-    points.push({ x: math.cos(angle), y: math.sin(angle) });
+type Level = number;
+type Draw = (Point, Point, Point) => Level => *;
+export const draw: Draw = (a, b, c) => (level) => {
+  if (level === 0) {
+    return [a, b, c];
   }
 
-  return points;
+  const next = level - 1;
+  const a2b = midPoint(a, b);
+  const b2c = midPoint(b, c);
+  const c2a = midPoint(c, a);
+
+  const ap = draw(a, a2b, c2a)(next); // bottom left
+  const bp = draw(a2b, b, b2c)(next); // top
+  const cp = draw(c2a, b2c, c)(next); // bottom right
+
+  return [ap, bp, cp];
 };
