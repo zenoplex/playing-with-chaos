5ad99e1e29708ddf46f9428d1bb6b711b0a74b99
Gen Ichino <zenoplex@gmail.com>
2017-12-01 01:20:51 +0900

    feat: add scaling

--- src/common/canvas.js
+++ src/common/canvas.js
@@ -4,6 +4,9 @@ import PIXI from 'pixi.js';
 type Setup = (*) => PIXI.Application;
 export const setup: Setup = (options) => {
   const app = new PIXI.Application(options);
+  app.renderer.autoResize = true;
+  app.view.style.width = '100%';
+  app.view.style.height = '100%';
   if (document.body) document.body.appendChild(app.view);
   return app;
 };
@@ -12,5 +15,11 @@ type Clear = PIXI.Application => PIXI.Application;
 export const clear: Clear = (app) => {
   app.stage.destroy();
   app.stage = new PIXI.Container();
+  return app;
+};
+
+type Resize = (number, number) => PIXI.Application => PIXI.Application;
+export const resize: Resize = (width, height) => (app) => {
+  app.renderer.resize(width, height);
   return app;
 };
