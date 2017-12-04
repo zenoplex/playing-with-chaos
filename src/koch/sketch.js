0739a94fda7f5fb158d378257feb193d8f4f9d26
Gen Ichino <zenoplex@gmail.com>
2017-12-05 01:45:32 +0900

    fix: maximum call stack

--- src/koch/sketch.js
+++ src/koch/sketch.js
@@ -11,7 +11,7 @@ const graphic = new PIXI.Graphics();
 app.stage.addChild(graphic);
 
 const maxDepth = 6;
-let depth = 1;
+let depth = 0;
 let mod = 1;
 
 const reset = () => {
@@ -23,7 +23,7 @@ const render = () => {
   const p0 = new Point(0, winHeight / 2);
   const p1 = new Point(winWidth, winHeight / 2);
 
-  if (depth === 1) mod = 1;
+  if (depth <= 0) mod = 1;
   else if (depth >= maxDepth) mod = -1;
   depth += mod;
   const points = koch.plot(p0, p1)(depth);
@@ -31,7 +31,7 @@ const render = () => {
   const len = points.length;
   for (let i = 0; i < len; i++) {
     const point = points[i];
-    graphic.lineStyle(1, 0xffffff);
+    graphic.lineStyle(2, 0xffffff);
     if (i === 0) graphic.moveTo(point.x, point.y);
     graphic.lineTo(point.x, point.y);
   }
